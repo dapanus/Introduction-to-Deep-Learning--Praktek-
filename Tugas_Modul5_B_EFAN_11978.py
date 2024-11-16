20c7486f00f23c0e242be9394d80825a3cde2b2c
dapanus <129003347+dapanus@users.noreply.github.com>
2024-11-16 16:32:33 +0700

    Update Tugas_Modul5_B_EFAN_11978.py

--- Tugas_Modul5_B_EFAN_11978.py
+++ Tugas_Modul5_B_EFAN_11978.py
@@ -4,8 +4,7 @@ import pickle
 from PIL import Image
 import os
 
-model_directory = r'D:\semester 5\Mesin Learning\Introduction to Deep Learning (Praktek)'
-model_path = os.path.join(model_directory, r'best_model.pkl')
+model_path = r'best_model.pkl'
 
 if os.path.exists(model_path):
     try:
